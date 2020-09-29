31dba07a09f27d0e7006aad16f76388b75104543
Kfir Cohen <kfir969@gmail.com>
2020-09-30 00:23:35 +0300

    Add support for unknown commands

--- handlers.py
+++ handlers.py
@@ -3,4 +3,6 @@ from telegram.ext import Filters
 
 
 def setup_handlers(bot):
-    pass
+    @bot.handle.message(Filters.command)
+    def unknown(update, context):
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
