e906ed8da2bc289ec212bfdd0c9f6524b920c276
Kfir Cohen <kfir969@gmail.com>
2020-09-30 00:24:06 +0300

    Add echo, caps, and inline caps commands

--- handlers.py
+++ handlers.py
@@ -3,6 +3,32 @@ from telegram.ext import Filters
 
 
 def setup_handlers(bot):
+    @bot.handle.message(Filters.text & (~Filters.command))
+    def echo(update, context):
+        context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+    @bot.handle.command()
+    def caps(update, context):
+        """Will return everything in caps"""
+
+        text_caps = ' '.join(context.args).upper() or '...'
+        context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+    @bot.handle.inline_query()
+    def inline_caps(update, context):
+        query = update.inline_query.query
+        if not query:
+            return
+        results = list()
+        results.append(
+            InlineQueryResultArticle(
+                id=query.upper(),
+                title='Caps',
+                input_message_content=InputTextMessageContent(query.upper())
+            )
+        )
+        context.bot.answer_inline_query(update.inline_query.id, results)
+
     @bot.handle.message(Filters.command)
     def unknown(update, context):
         context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
