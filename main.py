5a54d7f3555c56bb965d87e00d21c625b9a93743
Kfir Cohen <kfir969@gmail.com>
2020-09-30 18:06:11 +0300

    Remove TelegramBot dependancy on system environment

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-from os import path
+from os import path, environ
 from dotenv import load_dotenv
 
 from TelegramBot import TelegramBot
 
 from handlers import setup_handlers
 
-# load environment from .env file
-basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, '.env'))
 
+def load_and_validate_environment():
+    # load environment from .env file
+    basedir = path.abspath(path.dirname(__file__))
+    load_dotenv(path.join(basedir, '.env'))
+
+    for key in ('TELEGRAM_TOKEN', 'LOGGER_PATH'):
+        if key not in environ:
+            raise NotImplementedError(f"'{key}' environment variable isn't specified")
+
+
+load_and_validate_environment()
 bot = TelegramBot(
-    help_description="My name is KfirBot, I'm going to help you with everything you need!"
+    token=environ['TELEGRAM_TOKEN'],
+    logger_path=environ['LOGGER_PATH'],
+    help_description="My name is KfirBot, I'm going to help you with everything you need!",
 )
 setup_handlers(bot)
 
