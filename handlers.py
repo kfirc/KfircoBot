e05094503e3e1679df01dfe54e86fd2eafaad592
Kfir Cohen <kfir969@gmail.com>
2020-10-03 00:12:50 +0300

    Remove caps and inline caps handlers

--- handlers.py
+++ handlers.py
@@ -1,4 +1,4 @@
-from telegram import InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Filters
 
 from globals import owner
@@ -44,28 +44,6 @@ def setup_handlers(bot):
     def details(update, context):
         """Receive some personal details about the owner"""
         context.bot.send_message(chat_id=update.effective_chat.id, text=str(owner))
-
-    @bot.handle.command()
-    def caps(update, context):
-        """Will return everything in caps"""
-
-        text_caps = ' '.join(context.args).upper() or '...'
-        context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
-
-    @bot.handle.inline_query()
-    def inline_caps(update, context):
-        query = update.inline_query.query
-        if not query:
-            return
-        results = list()
-        results.append(
-            InlineQueryResultArticle(
-                id=query.upper(),
-                title='Caps',
-                input_message_content=InputTextMessageContent(query.upper())
-            )
-        )
-        context.bot.answer_inline_query(update.inline_query.id, results)
 
     @bot.handle.message(Filters.command)
     def unknown(update, context):
